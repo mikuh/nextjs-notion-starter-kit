54b5e1353df9e1547d66c36365581f8e0b9b219a
geb <853934146@qq.com>
2022-11-04 09:20:56 +0800

    add menu

--- site.config.ts
+++ site.config.ts
@@ -48,16 +48,16 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default',
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'AI绘画作品',
+      pageId: '8c71cc36451a47868f9330b1233464cd'
+    },
+    {
+      title: 'Contact',
+      pageId: '2095f3adca0e4c868188ff64e8220520'
+    }
+  ]
 })
